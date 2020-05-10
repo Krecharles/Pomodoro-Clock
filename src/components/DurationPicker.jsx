f83ef35bafc826444f74bd8bd35e64991b43f54f
Krecharles <charelctk@gmail.com>
2020-05-10 12:16:06 +0200

    Fixed Safari bug

--- src/components/DurationPicker.jsx
+++ src/components/DurationPicker.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
 export const DurationPicker = ({ setTimerDuration }) => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
   const handleChange = (e) => {
+    // e.preventDefault();
     const newValue = e.target.value;
-    setValue(newValue);
-    e.preventDefault();
 
-    if (newValue === "") setTimerDuration(25 * 60);
+    if (newValue === "") {
+      setTimerDuration(25 * 60);
+      setValue(newValue);
+      return;
+    }
+    if (isNaN(newValue) || newValue === 0) return;
+    setValue(newValue);
     const parsed = parseInt(newValue);
-    if (isNaN(parsed) || parsed === 0) return;
     setTimerDuration(parsed * 60);
   };
   return (
     <input
-      className="appearance-none my-4 placeholder-green-400 shadow bg-green-700 mx-8 w-40 py-3 rounded-md text-3xl text-green-400 focus:outline-none select-none text-center"
+      className="my-4 placeholder-green-400 shadow bg-green-700 mx-8 w-40 py-3 rounded-md text-3xl text-green-400 focus:outline-none text-center"
       placeholder="Custom"
       value={value}
       onChange={handleChange}
